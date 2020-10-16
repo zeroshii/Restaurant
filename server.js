aa8cc98c061a243e1cb0857f0d14d4faa716fd5c
Garman_K <garnetbirthstone@hotmail.com>
2020-10-16 12:15:44 -0400

    test

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000
 
 // enabling CORS
 app.use(cors());
